e374d9d3269e56e7b13afd5fa55ca4cdbe84c34d
seth mcfeeters <seth.mcfeeters@gmail.com>
2021-05-31 17:20:30 -0700

    added model form

--- todolist/views.py
+++ todolist/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from .models import Status, ToDo
+from .forms import ToDoForm
+from django.http import HttpResponseRedirect
 
 def home(request):
+    #post request from model form
+    form = ToDoForm()
+    if request.method == 'POST':
+        form = ToDoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/")
+        print(request.POST)
+    #request
     todos = ToDo.objects.all()
-    context = {'todos': todos}
+    context = {'todos': todos, 'form': form}
     return render(request, 'todolist/todos.html', context)
