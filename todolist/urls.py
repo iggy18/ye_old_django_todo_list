f77955ec9a2eb6662aef85ba7ff9e19f6097dceb
seth mcfeeters <seth.mcfeeters@gmail.com>
2021-06-01 21:34:18 -0700

    added delete

--- todolist/urls.py
+++ todolist/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('delete_todo/<todo_id>', views.delete_todo, name='delete_todo'),
 ]
